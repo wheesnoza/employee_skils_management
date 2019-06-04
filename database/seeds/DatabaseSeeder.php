a327ad4ca2cf2cae3263f7336862222130d54018
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-06-05 00:24:33 +0900

    Create user seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        /** 渡されたテーブルを一括削除する */
+        $this->truncateTables([
+           'users'
+        ]);
+
+        /** Seeder実行 */
+        $this->call(UsersTableSeeder::class);
+    }
+
+    /**
+     * テーブル一括削除
+     *
+     * @param array $tables
+     */
+    public function truncateTables(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
