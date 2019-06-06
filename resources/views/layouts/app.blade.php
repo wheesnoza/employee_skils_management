a9345d7e3db05dc1afe5da7d23b09275daef80e0
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-06-07 00:42:40 +0900

    Add menus to naviagtion bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,6 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -49,6 +46,12 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/">ホーム</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/users">社員一覧</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
