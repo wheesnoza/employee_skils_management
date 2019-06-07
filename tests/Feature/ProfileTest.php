2f898bc1fc14e2dc41322dc316b8b36818f56c67
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-06-07 10:22:15 +0900

    Add test case

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -10,7 +10,8 @@ use Tests\TestCase;
 class ProfileTest extends TestCase
 {
     /**
-     * 新規登録していないユーザはプロフィール登録はできない
+     * 新規登録していないユーザは
+     * プロフィール編集ページを見ることができない
      *
      * @test
      */
@@ -21,7 +22,9 @@ class ProfileTest extends TestCase
     }
 
     /**
-     * 新規登録していないユーザはプロフィール登録はできない
+     * 新規登録していてかつ
+     * ログインしているユーザは
+     * プロフィール編集ページを見ることができる
      *
      * @test
      */
@@ -39,4 +42,143 @@ class ProfileTest extends TestCase
             ->assertStatus(200)
             ->assertSee('プロフィール');
     }
+
+    /**
+     * 姓は必須
+     *
+     * @test
+     */
+    public function first_name_is_required_when_updating_profile()
+    {
+        $user = factory(User::class)->create();
+        factory(Profile::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->actingAs($user);
+
+        $this->assertTrue(Auth::check());
+
+        $this->from(route('profile.edit'))
+            ->put(route('profile.update'), [
+                'last_name' => 'Last name',
+                'address' => 'address',
+                'birth_year' => 1998,
+                'birth_month' => 3,
+                'birth_day' => 31,
+            ])
+            ->assertRedirect(route('profile.edit'))
+            ->assertSessionHasErrors(['first_name']);
+        $this->assertDatabaseMissing('profiles', ['last_name' => 'Last name']);
+    }
+
+    /**
+     * 名は必須
+     *
+     * @test
+     */
+    public function last_name_is_required_when_updating_profile()
+    {
+        $user = factory(User::class)->create();
+        factory(Profile::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->actingAs($user);
+
+        $this->assertTrue(Auth::check());
+
+        $this->from(route('profile.edit'))
+            ->put(route('profile.update'), [
+                'first_name' => 'First name',
+                'address' => 'address',
+                'birth_year' => 1998,
+                'birth_month' => 3,
+                'birth_day' => 31,
+            ])
+            ->assertRedirect(route('profile.edit'))
+            ->assertSessionHasErrors(['last_name']);
+        $this->assertDatabaseMissing('profiles', ['first_name' => 'First name']);
+    }
+
+    /**
+     * 住所は必須
+     *
+     * @test
+     */
+    public function address_is_required_when_updating_profile()
+    {
+        $user = factory(User::class)->create();
+        factory(Profile::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->actingAs($user);
+
+        $this->assertTrue(Auth::check());
+
+        $this->from(route('profile.edit'))
+            ->put(route('profile.update'), [
+                'first_name' => 'First name',
+                'last_name' => 'Last name',
+                'birth_year' => 1998,
+                'birth_month' => 3,
+                'birth_day' => 31,
+            ])
+            ->assertRedirect(route('profile.edit'))
+            ->assertSessionHasErrors(['address']);
+        $this->assertDatabaseMissing('profiles', ['first_name' => 'First name']);
+    }
+
+    /**
+     * 生年月日は必須
+     *
+     * @test
+     */
+    public function birth_is_required_when_updating_profile()
+    {
+        $user = factory(User::class)->create();
+        factory(Profile::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->actingAs($user);
+
+        $this->assertTrue(Auth::check());
+
+        $this->from(route('profile.edit'))
+            ->put(route('profile.update'), [
+                'first_name' => 'First name',
+                'last_name' => 'Last name',
+            ])
+            ->assertRedirect(route('profile.edit'))
+            ->assertSessionHasErrors(['birth_year', 'birth_month', 'birth_day']);
+        $this->assertDatabaseMissing('profiles', ['first_name' => 'First name']);
+    }
+
+    /**
+     * プロフィールの編集は可能
+     *
+     * @test
+     */
+    public function user_can_update_profile()
+    {
+        $user = factory(User::class)->create();
+        factory(Profile::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->actingAs($user);
+
+        $this->assertTrue(Auth::check());
+
+        $this->from(route('profile.edit'))
+            ->put(route('profile.update'), [
+                'first_name' => 'First name',
+                'last_name' => 'Last name',
+                'address' => 'address',
+                'birth_year' => 1998,
+                'birth_month' => 3,
+                'birth_day' => 31,
+            ])
+            ->assertRedirect(route('home'));
+
+        $this->assertDatabaseHas('profiles', ['first_name' => 'First name']);
+    }
+
 }
