3778ecdc08f1060caabae55438241c212ed0b344
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-06-07 17:43:16 +0900

    Add timeline for users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-4">
+            <a href="{{ route('profile.edit') }}" class="btn btn-info btn-lg text-white ml-2 mb-3 shadow">プロフィール編集</a>
             <div class="card shadow mb-3">
                 <div class="card-body">
                     <img src="@if($user->profile->avatar) /images/{{ $user->profile->avatar }} @else {{ asset('storage/images/no_avatar.png') }} @endif" alt="..." class="img-thumbnail mb-3">
@@ -11,9 +12,9 @@
                     <p>{{ $user->profile->address }}</p>
                     <p>{{ $user->profile->birth_year }}/{{ $user->profile->birth_month }}/{{ $user->profile->birth_day }}</p>
                     <p>{{ $user->email }}</p>
-                    <a href="{{ route('profile.edit') }}" class="btn btn-info btn-lg text-white">プロフィール編集</a>
                 </div>
             </div>
+            <a href="{{ route('profile.edit') }}" class="btn btn-info btn-lg text-white ml-2 mb-3 shadow">スキルを追加する</a>
             <div class="row">
                 <div class="col-md-6">
                     <div class="card shadow mb-3">
@@ -50,13 +51,32 @@
             </div>
         </div>
         <div class="col-md-8 text-right">
-            <div class="card shadow">
-                <br><br><br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br><br><br><br><br>
-            </div>
+            <a href="{{ route('profile.edit') }}" class="btn btn-info btn-lg text-white ml-2 mb-3 shadow">経歴を追加する</a>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-badge info"></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="timeline-title">プロジェクト名</h4>
+                                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 2018-2019</small></p>
+                            </div>
+                            <div class="timeline-body mb-3">
+                                <p>
+                                    コンテンツの内容ではなく、純粋にデザインを評価する場合にこれらの文章は役にたちます。
+                                    人間は文章を読んでその意味を理解してしまうと、その他の部分への評価に影響を与えてしまうと言われていることがその理由です。
+                                    では、日本語の場合はどうでしょう。 前述のラテン文字とは違い、日本語の表記体系は非常に複雑です。
+                                    ひらがな・カタカナ・漢字・数字・アルファベットを織り交ぜて記述されるため、Lorem ipsumでは全く代用できません。
+                                </p>
+                            </div>
+                            <div class="btn-group">
+                                <a href="#" class="btn btn-danger">削除</a>
+                            </div>
+                            <div class="btn-group">
+                                <a href="#" class="btn btn-primary">編集</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
         </div>
     </div>
 </div>
